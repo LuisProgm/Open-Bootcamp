73081b8001121625d42a076b71e04c36b472d25c
LuisProgm <luisx003@gmail.com>
2022-10-26 23:12:07 -0500

    Finish ejercicio 02

---
+++ Javascript-Basico/Ejercicio_02/ejercicio2.js
@@ -0,0 +1,14 @@
+const lista = [
+    'Luis', 
+    24, 
+    true, 
+    new Date(1998, 01 ,03), 
+    {
+    titulo: 'La vuelta al mundo en 80 dias',
+    autor: 'Julio Verne',
+    fecha: new Date(1872, 10, 07),
+    url: 'https://www.casadellibro.com/libro-la-vuelta-al-mundo-en-80-dias/9788466716550/859804'
+    }
+];
+
+
