acd4cdcdd9f3b31c9a0d3021dbf04e784df80d49
LuisProgm <luisx003@gmail.com>
2022-10-27 19:53:31 -0500

    Finish ejercicio 03

--- Javascript-Basico/Ejercicio_03/factorial-for.js
+++ Javascript-Basico/Ejercicio_03/factorial-for.js
@@ -1,7 +1,7 @@
 // Ejercicio 03 :
 // factorial-for.js -> Este archivo debe calcular el factorial de 10 utilizando un solo bucle for.
 
-let numero = 15; 
+let numero = 15;
 let factorial = 1;
 
 for (let i = 1; i <= numero; i++) {
