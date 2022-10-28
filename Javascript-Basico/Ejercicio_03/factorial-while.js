07b601809ec098e5df69a0ed2a373088a694d622
LuisProgm <luisx003@gmail.com>
2022-10-27 19:51:21 -0500

    Finish ejercicio 03

--- Javascript-Basico/Ejercicio_03/factorial-while.js
+++ Javascript-Basico/Ejercicio_03/factorial-while.js
@@ -1,7 +1,7 @@
 // Ejercicio 03
 // factorial-while.js -> Este archivo debe calcular el factorial de 10 utilizando un bucle while.
 
-let numero = 15;
+let numero = 15; 
 let factorial = 1;
 let i = 1;
 
